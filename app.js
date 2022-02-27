43359f4288cd6f1a470f1a371aa2e3a19d3101a7
Miyka96 <micaelaseminara@outlook.it>
2022-02-27 19:50:56 +0100

    main functions done

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const app = new Vue ({
-    el : '#root',
+    el: '#root',
     data: {
-
+        todoInput: '',
+        todoList: 
+        ['Fare cose',
+        'Fare altre cose',
+        'Fare altre cose ancora'],
     },
     methods: {
-
+        addNewTodo: function() {
+            this.todoList.push(this.todoInput)
+            this.todoInput= '';
+        },
+    
+        deleteTodo: function(i){
+            this.todoList.splice(i,1);
+        }
     },
 })
